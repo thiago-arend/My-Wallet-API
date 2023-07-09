cf68f3d8a6a6f684303c4a0ccfd2cfbc5684ef68
thiago-arend <thiago.arend@gmail.com>
2023-07-09 20:50:43 -0300

    deleção de arquivos desnecessários

--- src/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getUser } from "../controllers/user.controller.js";
-import { validateAuth } from "../middlewares/validateAuth.js";
-
-const userRouter = Router();
-userRouter.get("/user", validateAuth, getUser);
-
-export default userRouter;
