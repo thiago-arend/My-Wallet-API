a0921199241c646981510a2498a071a4a958b893
thiago-arend <thiago.arend@gmail.com>
2023-07-09 20:51:32 -0300

    deleção de arquivos desnecessários

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import userRouter from "./user.routes.js";
 import transactionRouter from "./transaction.routes.js";
 import authRouter from "./auth.routes.js";
 
 const router = Router();
 router.use(authRouter);
-router.use(userRouter);
 router.use(transactionRouter);
 
 export default router;
